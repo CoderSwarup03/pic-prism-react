f3d692010106db77ec91430d5cf0d4e0d5ec2cbe
CoderSwarup03 <swarupmahata321@gmail.com>
2024-08-27 15:55:01 +0530

    day6 connected with DB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@
 // -> 1. Express ko bulana padega && 
 const express = require('express');
 const dotenv = require('dotenv');
+const { connetDb } = require("./connection");
+// readdirSync used for folder stracture read
+const {readdirSync} = require('fs')
+
+
+// const authRoute = require("./routes/authRoutes")
+
 
 // bindind env files
 dotenv.config();
@@ -12,12 +19,30 @@ const app = express();
 // -> 3. Port define karna padega(means like a door)
 const port = process.env.PORT || 8000;
 
+// app.use("/api",authRoute);
+
+
+
 // -> 4. Making routes
-app.get("/",(req, res) =>{
+app.get("/", (req, res) => {
     res.send("Hellow Server");
 })
 
+
+connetDb();
+
+// how to use routes
+// app.use("/api", authRoute);
+// call readdirSync 
+
+// importing and using routes dynmamically
+readdirSync("./routes").map((route) => {
+    app.use("/api",require(`./routes/${route}`))
+})
+
+// console.log(readdirSync("./routes"))
+
 // -> 5. Server start karna padega
-app.listen(port,()=> {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,14 @@
+// One Way
+// const express = require("express");
+// const router = express.Router();
+
+const { login, signup } = require("../controllers/authController");
+
+
+// another way
+const router = require("express").Router();
+
+router.post("/login", login)
+router.post("/signup", signup)
+
+module.exports = router;
